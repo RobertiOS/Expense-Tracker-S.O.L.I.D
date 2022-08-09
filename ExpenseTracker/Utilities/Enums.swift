8efb7484450af7933bf3d721c03ba6a584bb2153
Robert <robert.ios.developer@gmail.com>
2022-08-08 22:30:12 -0600

    :technologist: added weekly report

--- ExpenseTracker/Utilities/Enums.swift
+++ ExpenseTracker/Utilities/Enums.swift
@@ -3,7 +3,8 @@ import Foundation
 enum ReportRange: String, CaseIterable {
 case monthly = "This month"
 case daily = "Today"
-
+case weekly = "This week"
+  
   func timeRange() -> (Date, Date) {
     let now = Date()
     switch self {
@@ -11,6 +12,8 @@ case daily = "Today"
       return (now.startOfMonth, now.endOfMonth)
     case .daily:
       return (now.startOfDay, now.endOfDay)
+    case .weekly:
+      return (now.startOfWeek, now.endOfWeek)
     }
   }
 }
